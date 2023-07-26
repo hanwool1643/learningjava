03357a595d325ed4673f2e8b7aee02d0e877c84b
han YEO <hanwe92@gmail.com>
2023-07-27 07:05:02 +0900

    first commit

---
+++ src/main/java/com/firstSpring/firstProject/RestApiDemoController.java
@@ -0,0 +1,17 @@
+package com.firstSpring.firstProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestApiDemoController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres Pontas")
+        ));
+    }
+}
---
+++ src/test/java/com/firstSpring/firstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstSpring.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
