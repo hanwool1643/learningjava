c37f21cb73ef0cd0380e542485b21146c47ec6c0
han YEO <hanwe92@gmail.com>
2023-07-29 18:30:56 +0900

    programmers/functional interface 활용 사례

--- src/main/java/com/programmers/java/func/Main.java
+++ src/main/java/com/programmers/java/func/Main.java
@@ -1,7 +1,19 @@
 package com.programmers.java.func;
 
+class Greeting implements MySupply {
+    @Override
+    public String supply() {
+        return "Hello World";
+    }
+}
+class SayHello implements  MyRunnable {
+    @Override
+    public void run() {
+        System.out.println(new Greeting().supply());
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        new SayHello().run();
     }
 }
