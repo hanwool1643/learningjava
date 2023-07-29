a76409f8e34a3238d0acb4b20b60294460da7718
han YEO <hanwe92@gmail.com>
2023-07-29 14:22:35 +0900

    programmers/default method-interface 추가 만으로 기능 확장

--- src/main/java/com/programmers/java/def2/Main.java
+++ src/main/java/com/programmers/java/def2/Main.java
@@ -2,11 +2,12 @@ package com.programmers.java.def2;
 
 class Duck implements Swimmable, Walkable {}
 
-class Swan implements Flyable, Walkable {}
+class Swan implements Flyable, Walkable, Swimmable {}
 public class Main {
     public static void main(String[] args) {
         new Duck().walk();
         new Duck().swim();
         new Swan().fly();
+        new Swan().swim();
     }
 }
