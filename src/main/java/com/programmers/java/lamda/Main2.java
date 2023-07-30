d03bcc82b892d60035dcf1d81f28c5bf87287ed0
han YEO <hanwe92@gmail.com>
2023-07-30 15:47:06 +0900

    programmers/함수형 인터페이스-javdoc에 미리 정해진 함수형 인터페이스 사용해서 구현을 호출에 맡기기

--- src/main/java/com/programmers/java/lamda/Main2.java
+++ src/main/java/com/programmers/java/lamda/Main2.java
@@ -1,10 +1,23 @@
 package com.programmers.java.lamda;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class Main2 {
     public static void main(String[] args) {
-        new Main2().loop(10, i -> System.out.println(i));
+//        new Main2().loop(10, i -> System.out.println(i));
+        new Main2().filteredNumbers(30,
+                i -> i % 2==0,
+                System.out::println
+                );
     }
 
+    void filteredNumbers(int max, Predicate<Integer> p, Consumer<Integer> c) {
+        for(int i = 0; i<max ; i++) {
+            if(p.test(i)) c.accept(i);
+        }
+
+    }
     void loop(int n, MyConsumer<Integer> consumer) {//MyConsumer쪽(외부에) 의존성을 맡김
         for(int i=0; i<n; i++) {
             consumer.consume(i);
